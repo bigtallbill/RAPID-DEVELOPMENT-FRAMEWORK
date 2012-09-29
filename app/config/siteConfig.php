ee9bcfd2b6c67432977a1186490e5f190d99e036
Alan Hamlyn <alan.hamlyn@gmail.com>
2012-09-29 19:53:24 -0400

    Site config added

---
+++ app/config/siteConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+// site config variables
+
+define('APP_VERSION', '0.1');
+define('APP_NAME', 'YOUR APP NAME');
+define('APP_DOMAIN', 'http://urlhere.com');
+define('APP_URL', 'http://'.$_SERVER['HTTP_HOST']);
+define('APP_BASEPATH', '/app/tmpl/');
+define('APP_FULLPATH', APP_URL.APP_BASEPATH);
+
+// define more
+//define('', '');
+
+?>
