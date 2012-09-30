d36298b78cf4fa08d31c220943547e825a77b914
Alan Hamlyn <alan.hamlyn@gmail.com>
2012-09-30 13:57:54 -0400

    redundant

--- app/config/siteConfig.php
+++ app/config/siteConfig.php
@@ -4,7 +4,6 @@
 
 define('APP_VERSION', '0.1');
 define('APP_NAME', 'YOUR APP NAME');
-define('APP_DOMAIN', 'http://urlhere.com');
 define('APP_URL', 'http://'.$_SERVER['HTTP_HOST']);
 define('APP_BASEPATH', '/app/tmpl/');
 define('APP_FULLPATH', APP_URL.APP_BASEPATH);
